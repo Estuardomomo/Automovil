828cb5c0648821d6589ec42c5593fd4db6520771
oswilehi <oswilehi@gmail.com>
2018-11-24 14:11:12 -0600

    ident changes

--- RaspberryPi-Movil.py
+++ RaspberryPi-Movil.py
@@ -2,7 +2,9 @@ import sys
 import time
 import Adafruit_DHT
 
-while True:
+contadorGlobal = 0
+
+while contadorGlobal < 3:
  #------------------------------------CÓDIGO DE LOS MOTORES, MOVERSE A LA POSICIÓN ------------------------------------
  
  #---------------------------------CÓDIGO DE LOS SENSORES (HUMEDAD,TEMPERATURA Y LUZ)----------------------------------
@@ -10,12 +12,14 @@ while True:
     pin = 23                    #Configuracion del puerto GPIO al cual esta conectado (GPIO 23)
     try:                        
 	    contador = 0
-        while contador < 24     #Debemos medir datos 2 minutos, y tardamos 5 seg por medición.
+        while contador < 60     #Debemos medir datos 2 minutos, y tardamos 2 seg por medición.
 		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 		#Imprime en la consola las variables temperatura y humedad con un decimal
 		print('Temperatura={0:0.1f}*  Humedad={1:0.1f}%'.format(temperatura, humedad))
-        #Duerme 5 segundos
-		time.sleep(5)
+        #Duerme 2 segundos
+		time.sleep(2)
+        contador = contador + 1
     except Exception,e: 
-	print str(e) # Imprime en pantalla el error e
+	    print str(e) # Imprime en pantalla el error e
  #----------------------------------CÓDIGO DE LOS MOTORES, REGRESAR AL ORIGEN (0,0)-------------------------------------
+    contadorGlobal = contadorGlobal + 1
